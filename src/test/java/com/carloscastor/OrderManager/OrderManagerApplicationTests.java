df1ea747659a1ea5c4f7d467c743e0d299929b95
Carlos Castor <carlos.castor.dev@gmail.com>
2022-10-19 20:32:54 -0300

    first version

---
+++ src/test/java/com/carloscastor/OrderManager/OrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloscastor.OrderManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
